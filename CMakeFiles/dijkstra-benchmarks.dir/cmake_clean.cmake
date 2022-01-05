249222eb6ba2f9d6db2b84529e0974804561c026
Your Name <you@example.com>
2022-01-05 11:04:37 +0100

    cmake issues fixed

---
+++ CMakeFiles/dijkstra-benchmarks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dijkstra-benchmarks.dir/main.cpp.o"
+  "dijkstra-benchmarks"
+  "dijkstra-benchmarks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dijkstra-benchmarks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/dijkstra-benchmarks.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dijkstra-benchmarks.
+# This may be replaced when dependencies are built.
